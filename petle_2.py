8139afbfead9847921951dd3165def0df14fcf37
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-03-21 13:44:27 +0100

    Add files via upload
    petle part 2

--- petle_2.py
+++ petle_2.py
@@ -1,11 +1,16 @@
 lista = [1, 3, 14, 56, 78, 90]
-
+# petla for po elementach
 for cyfra in lista:
     if cyfra == 3:  # przyrownanie, czyli sprawdzenie czy równa sie 2
         cyfra += 1  # cyfra = cyfra + 1
     print(cyfra)
 
-list_3 = [j for j in range(10) if j % 2 == 0]
+# rownowazne petle for
+# for i in range(10):  # 0..9
+#     if i % 2 == 0:  # reszta z dzielenia
+#         print(i)
+#         lista_parzyste.append(i)
+list_3 = [j for j in range(10) if j % 2 == 0]  # skrotowiec
 print(list_3)
 
 imiona = ["Radek", "Zenek", "Marta"]
@@ -13,10 +18,11 @@ for p in imiona:  # petla po liscie stringow
     print(p)
 
 # 0 Radek, 1 Zenek
-
+# wypisanie elementow z listy str z indeksem i wartoscia
 for p in range(len(imiona)):  # range(3) len = 3, czyli 0..2
     print(p + 1, imiona[p])  # imiona[p] - pobranie elementu z listy po indeksie
 
+# uzycie enumerate zamiast iterowac po długosci listy
 for p, w in enumerate(imiona):  # p -index, w = imiona[p]
     print(p + 1, w)
 
@@ -28,9 +34,11 @@ for p, o in enumerate(ludzie):
 for p, o in enumerate(ludzie):
     print(p, o, wiek[p])
 
+# złaczenie dwoch list w jedna
 for l, w in zip(ludzie, wiek):
     print(l, w)
-    
+# złaczenie i dodanie indeksu
 ind = list(range(len(ludzie)))
 for i, l, w in zip(ind, ludzie, wiek):
     print(i, l, w)
+# 13:31
