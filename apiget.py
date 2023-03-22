edb0d4ddac9967fa0d579e3be92d20d23f18bf56
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-03-22 16:22:57 +0100

    Add files via upload
    apiget

---
+++ apiget.py
@@ -0,0 +1,14 @@
+import requests as re
+
+# pip install request
+url = "http://api.nbp.pl/api/exchangerates/rates/A/USD/"
+response = re.get(url)
+print(response)
+# 200 ok
+# 4 ..bledy
+# 5 bledy serwera
+table = response.json()
+print(table)
+print(table['rates'])
+print(table['rates'][0])
+print(table['rates'][0]['mid'])
